8f52b29ff146903f83917c626be918f5701f6038
maurawahl <mjw4048@utulsa.edu>
2024-03-14 15:09:56 -0500

    number 4 done

--- Problem 3/courseGrade.py
+++ Problem 3/courseGrade.py
@@ -1,10 +1,17 @@
-def courseGrade():
-    
+'''
+Name: Maura Wahl
+Time:Thursdays at 2pm
+'''
+def courseGrade(): #when spacing them out, do /t to format it
+
     # TODO: Declare any necessary variables here. 
-      
+    user_input = input()
+    filename = ("./Problem 3/" + str(user_input) + ".")
       
     # TODO: Read a file name from the user and read the tsv file here. 
-   
+    file_object = open(filename)
+    read_file = file_object.readlines()
+    print(read_file)
    
     # TODO: Compute student grades and exam averages, then output results to a text file here. 
     return
--- Problem 4/fileNameChange.py
+++ Problem 4/fileNameChange.py
@@ -1,6 +1,19 @@
+'''
+Name: Maura Wahl
+Time: Thursdays at 2pm
+'''
 def fileNameChange():
     #type your code here
+    filename = input()
+    with open(filename, "r") as file_object:
+        another_file_thing = file_object.readlines()
+    for x in another_file_thing:
+        x = x.strip()
+        y = x.replace("photo.jpg","info.txt")
+        print(y)
+    
 
+        
     return
 
 if __name__ == '__main__':
